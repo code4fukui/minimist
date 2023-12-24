078cad20c23ddc3388220e17bac6a54b1464ab8d
Taisuke Fukuno <fukuno@jig.jp>
2023-12-24 09:05:39 +0900

    use tape-es

--- test/tape.js
+++ test/tape.js
@@ -1,10 +1,1 @@
-import * as _t from "https://deno.land/std/testing/asserts.ts";
-
-const t = {
-	deepEqual: _t.assertEquals,
-	same: _t.assertEquals,
-	equal: _t.assertEquals,
-	end: () => {},
-  plan: () => {},
-};
-export const test = (name, func) => Deno.test(name, () => func(t));
+export { test } from "https://code4fukui.github.io/tape-es/tape.js";
